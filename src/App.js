8e32cf4026378e1c6aaf3c7126abfbe191a697bf
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-08-25 23:48:32 +0300

    components delete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">
-          <h1>calculator Operand</h1>
-        </div>
-        <div className="current-operand">
-        <h1>calculator current Operand</h1>
-
-        <Calculator/>
-        </div>
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
